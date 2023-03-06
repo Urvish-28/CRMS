c5e73a2834e2d53c34282df121c3bbff380d3175
Urvish-28 <ubutani@aakashinformatics.com>
2023-03-06 11:01:52 +0530

    Add project files.

---
+++ CRMS1.Core/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRMS1.Core.Models
+{
+    public class User 
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Contact { get; set; }
+        public string Role { get; set; }
+    }
+}
